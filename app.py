cf03a1dc32124592820e49aa67d3387969c3a7ed
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-05-08 17:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "I love you kututpatutu"
+    return "Hello from Ajeem"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
